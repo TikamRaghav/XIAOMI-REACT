3b4d3e47350698c41b07df8a57c04a691de308a7
TikamRaghav <teekamraghav7248@gmail.com>
2024-10-26 22:17:22 +0530

    apple app

---
+++ src/Navbar.js
@@ -0,0 +1,79 @@
+function Navbar (){
+    return(
+        <div className="navbar1">
+            <div className="navbar">
+
+           
+            <div className="apple">         
+            <ul>
+                <li><img src="https://cdn-icons-png.flaticon.com/512/0/747.png" alt="" /></li>
+                <li>Store</li>
+                <li>Mac</li>
+                <li>ipad</li>
+                <li>iphon</li>
+                <li>Watch</li>
+                <li>AirPods</li>
+                <li>TV & Home</li>
+                <li>Entertainment</li>
+                <li>Accessories</li>
+                <li>Support</li>
+                <li><img src="https://cdn-icons-png.flaticon.com/128/3031/3031293.png" alt="" /></li>
+                <li><img src="https://cdn-icons-png.flaticon.com/128/11530/11530382.png" alt="" /></li>
+                </ul>           
+            
+            </div>
+
+            <div className="up">
+                <ul>
+                    Celebrate with up to ₹10000.00 instant cashback with eligible cards.* Plus up to 12 months of No Cost EMI.‡ Shop
+                </ul>
+            </div>
+            </div>
+
+            <div className="ipad">
+                <div className="mini">
+                    iPad mini
+                </div>
+               <div className="build">
+               Build for Apple Intelligence
+               </div>
+               <div className="learn">
+               <div className="more">
+               Learn more
+                </div> 
+                <div className="buy">
+                Buy
+                </div>
+               </div>
+
+           
+            <div className="tab">
+                <img src="https://www.apple.com/sg/ipad-mini/images/meta/ipad-mini_overview__ficvb3cxycmm_og.png" alt="" />
+            </div>
+            </div>
+            <div className="pro">
+                
+                <div className="learn">
+               <div className="more">
+               Learn more
+                </div> 
+                <div className="buy">
+                Buy
+                </div>
+                
+               </div>
+               <div className="us">
+                Apple intellegence startimg US English later year'
+                </div>
+            </div>
+
+            
+            
+            
+        </div>
+        
+        
+
+    )
+}
+export default Navbar;
